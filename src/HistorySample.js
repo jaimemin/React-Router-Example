c51c631ea96db61d83ea287661988e0fbc158882
jaimemin <jaimemin@gmail.com>
2020-04-17 09:51:45 +0900

    라우터 부가기능

---
+++ src/HistorySample.js
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react';
+
+function HistorySample({ history }) {
+    const goBack = () => {
+        history.goBack();
+    }
+
+    const goHome = () => {
+        history.push('/');
+    }
+
+    const replaceToHome = () => {
+        history.replace('/');
+    }
+
+    useEffect(() => {
+        console.log(history);
+        const unblock = history.block('정말 떠나실건가요?');
+
+        return () => {
+            unblock();
+        }
+    }, []);
+
+    return (
+        <div>
+            <button onClick={goBack}>뒤로 가기</button>
+            <button onClick={goHome}>홈으로</button>
+        </div>
+    );
+}
+
+export default HistorySample;
